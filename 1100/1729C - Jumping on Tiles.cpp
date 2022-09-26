f099ffc5a025a32df891cff6902b89376e5481d5
ayoubdkhissi <1999dkhissi@gmail.com>
2022-09-26 23:07:15 +0100

    adding 1260A

--- 1100/1729C - Jumping on Tiles.cpp
+++ 1100/1729C - Jumping on Tiles.cpp
@@ -52,10 +52,6 @@ string operator*(const string& s, ll n) {
 	1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
 	a b c d e f g h i j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
 
-	15 - 12 + 15 - 9 +
-
-	logic
-
 
 	• the minimum cost = |index( s.front - index(s.back) )|
 	• to get this minimum, 
