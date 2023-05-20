b5911b8b85862031a86f25fa27f8b4bc719c8aa9
Ayoub Dkhissi <1999dkhissi@gmail.com>
2023-05-20 14:04:49 +0000

    Update 1916B - Grid Construction.cs

--- 1000/1916B - Grid Construction.cs
+++ 1000/1916B - Grid Construction.cs
@@ -3,36 +3,35 @@ const bool MULTIPLE_TEST_CASES = true;
 /*
  * Notes: 
  * the answer is to simply construct the following grid:
- * 2n-1  2     2n-2 ........ n
- *  1   2n-3    3  ........ 2n
+ * 2n-1  2     n+2 ........2n-2 n
+ *  1   n+1    3   ........n-1  2n
 */
 
 static void Solve()
 {
-    long n = long.Parse(Console.ReadLine());
+    int n = int.Parse(Console.ReadLine());
 
     // grid of 2.n
-    long[,] grid = new long[2, n];
+    int[,] grid = new int[2, n];
 
     grid[0,0] = 2 * n - 1;
     grid[1, n - 1] = 2 * n;
 
-    long z = 1;
-    for(long i=0; i<n; i++)
+    int z = 1;
+    for(int i=0; i<n; i++)
     {
         grid[z, i] = i + 1;
         z = 1 - z;
     }
 
-    z = 0;
-    for (long i = 2; i<n-1; i+=2)
+    z = 1;
+    for (int i = 1; i<n-1; i++)
     {
-        grid[0, i] = 2*n - 2 - z;
-        grid[1, i - 1] = 2*n - 2 - z - 1;
-
-        z += 2;
+        grid[z, i] = n+i;
+        
+        z = 1 - z;
     }
-
+    
     // print grid
     for (int j = 0; j < 2; j++)
     {
@@ -42,7 +41,7 @@ static void Solve()
     }
 }
 
-long t = 1;
+int t = 1;
 if (MULTIPLE_TEST_CASES)
     t = int.Parse(Console.ReadLine());
 
